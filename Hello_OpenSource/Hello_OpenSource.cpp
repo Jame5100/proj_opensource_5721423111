3c4607247a81bc5d31f1167a14d908135226fc0e
Jame5100 <chanon105@hotmail.com>
2016-11-03 20:05:02 +0700

    Add project files.

---
+++ Hello_OpenSource/Hello_OpenSource.cpp
@@ -0,0 +1,17 @@
+#include "stdafx.h"
+
+#include <iostream>
+
+using namespace std;
+
+int _tmain(int argc, _TCHAR* argv[])
+
+{
+
+	cout << "Hello My Name is Takon Rodsomboon 5721423111";
+
+	system("pause");
+
+	return 0;
+
+}
